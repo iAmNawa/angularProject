389e06a6e4bfcd9d7db4dd4ead6bd63a3ac17b33
Paul Borawski <paulborawski14@gmail.com>
2016-07-07 16:23:44 -0700

    red x and green check working

--- quizApp/js/quiz.js
+++ quizApp/js/quiz.js
@@ -21,7 +21,7 @@
 		 if( questionState != 'answered') {
 			 $scope.myQuestions[qIndex].selectedAnswer = aIndex;
 			 var correctAnswer = $scope.myQuestions[qIndex].correct;
-
+       $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
 			 if( aIndex === correctAnswer ){
 				 $scope.myQuestions[qIndex].correctness = 'correct';
 				 $scope.score += 1;
@@ -30,8 +30,14 @@
 			 }
 			 $scope.myQuestions[qIndex].questionState = 'answered';
 		 }
+	 }
 
-		}
+	 $scope.isSelected = function(qIndex, aIndex){
+		 return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+	 }
+	 $scope.isCorrect = function(qIndex, aIndex){
+		 return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+	 }
 
 	}]);
 
